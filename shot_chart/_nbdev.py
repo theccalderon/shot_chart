42245769fe1230e7395dd6673885ddf46d5e0ecd
theccalderon <cesar.calderon.dev@gmail.com>
2020-02-03 14:29:30 -0500

    figsize

--- shot_chart/_nbdev.py
+++ shot_chart/_nbdev.py
@@ -10,12 +10,15 @@ index = {"Config": "00_core.ipynb",
          "untar_data": "00_core.ipynb",
          "make_df": "00_core.ipynb",
          "list_game_ids": "00_core.ipynb",
+         "Y_MODIFIER": "00_core.ipynb",
+         "X_MODIFIER": "00_core.ipynb",
          "plot_winner": "00_core.ipynb",
          "plot_loser": "00_core.ipynb",
          "list_team_players": "00_core.ipynb",
          "plot_player_full": "00_core.ipynb",
          "least_effective_shot_team": "00_core.ipynb",
-         "least_effective_shot_league": "00_core.ipynb"}
+         "least_effective_shot_league": "00_core.ipynb",
+         "most_effective_shot_league": "00_core.ipynb"}
 
 modules = ["core.py"]
 
